3aaccda0ad031cfc1c06d69fb9916e63f34a7f4c
Jolusegun <Joyce.o.olusegun@gmail.com>
2021-09-23 06:50:00 -0500

    Added Search Engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello Jesus</header>
+      <div className="container">
+        <header className="App-header">
+          <strong>Dictionary</strong>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Joyce Oluwatosin Olusegun</small>
+        </footer>
+      </div>
     </div>
   );
 }
